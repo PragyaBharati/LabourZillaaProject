5f7fe01550bbfb8f13fc680821516cc115408716
vishal kumar pandey <pragyabharati31@gmail.com>
2021-04-08 22:26:22 +0530

    Commit

--- Models/Labour.cs
+++ Models/Labour.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
@@ -9,21 +10,31 @@ namespace LabourZillaZoneee.Models
 {
     public partial class Labour
     {
+       
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Pswd { get; set; }
+        //[Compare("Pswd", ErrorMessage = "Password and Confirmation Password must match.")]
         public string Cnfrmpswd { get; set; }
         public string Profession { get; set; }
+        
         public string CityAddress { get; set; }
         public string StateL { get; set; }
+        //[Required(ErrorMessage ="required field")]
         public int? DailyWages { get; set; }
+        //[Required(ErrorMessage = "required field")]
+
         public DateTime? TimeDate { get; set; }
+      
+
         public string Available { get; set; }
+        [Required(ErrorMessage = "required field")] 
         public string Lcontact { get; set; }
         public string Ppic { get; set; }
         [NotMapped]
         public IFormFile PPicFile { get; set; }
+       
         public string RoleL { get; set; }
         
     }
